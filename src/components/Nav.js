f15bfa194daf4e5f72f11b219caf3d83c5f378dd
Brian Murray <brianmurraysound@gmail.com>
2021-08-23 17:33:36 -0300

    Nav routing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,30 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <StyledNav>
       <h1>
-        <a id="logo" href="#">
+        <Link id="logo" to="/">
           Captures
-        </a>
+        </Link>
       </h1>
       <ul>
         <li>
-          <a href="#">1. About Us</a>
+          <Link id="logo" to="/">
+            1. About Us
+          </Link>
         </li>
         <li>
-          <a href="#">2. Our Work</a>
+          <Link id="logo" to="/work">
+            2. Our Work
+          </Link>
         </li>
         <li>
-          <a href="#">3. Contact Us</a>
+          <Link id="logo" to="/contact">
+            3. Contact Us
+          </Link>
         </li>
       </ul>
     </StyledNav>
