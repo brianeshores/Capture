4ef9c42359748e448f2ed02fb42ad424f0cd71be
Brian Murray <brianmurraysound@gmail.com>
2021-08-23 10:28:28 -0300

    services section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,8 +1,14 @@
 import React from "react";
+import ServicesSection from "../components/ServicesSection";
 //import page components
 import AboutSection from "../components/AboutSection";
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+    </>
+  );
 };
 
 export default AboutUs;
