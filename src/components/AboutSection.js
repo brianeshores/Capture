56ddfcb5ddc8a2d31d5efe6d540bb052968b8ca6
Brian Murray <brianmurraysound@gmail.com>
2021-08-26 18:25:59 -0300

    About section animations

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import home1 from "../img/home1.png";
 import { About, Description, Hide, Image } from "../styles";
 import { motion } from "framer-motion";
+import { titleAnimation, fade, photoAnim } from "../animation";
 
 const AboutSection = () => {
   return (
@@ -9,25 +10,25 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnimation}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnimation}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="Guy with a camera" />
+        <motion.img variants={photoAnim} src={home1} alt="Guy with a camera" />
       </Image>
     </About>
   );
