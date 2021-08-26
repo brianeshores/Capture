b23de2c614a6d593e956531bed38c87d4726d58e
Brian Murray <brianmurraysound@gmail.com>
2021-08-26 14:20:56 -0300

    Page transition animations

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -4,14 +4,21 @@ import React from "react";
 import ServicesSection from "../components/ServicesSection";
 import FaqSection from "../components/FaqSection";
 import AboutSection from "../components/AboutSection";
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
 
 const AboutUs = () => {
   return (
-    <>
+    <motion.div
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+      exit="exit"
+    >
       <AboutSection />
       <ServicesSection />
       <FaqSection />
-    </>
+    </motion.div>
   );
 };
 
